7fbf0f2b338e089360e8bd1d32b92fa8e4012f02
Roman Enner <ennerra@yandexlyceum.ru>
2024-03-18 19:22:00 +0300

    for glitch

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import datetime
 from forms.user import RegisterForm, LoginForm
 from forms.news import NewsForm
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import os
 
 
 app = Flask(__name__)
@@ -147,7 +148,9 @@ def index():
 
 def main():
     db_session.global_init("db/blogs.db")
-    app.run(port=8080, host='127.0.0.1')
+    # app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
